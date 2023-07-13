0147f20e5e4b1c0867c1b903e69036fa7c810f41
JanSafronov <jansafronov@gmail.com>
2023-07-14 01:47:46 +0300

    Set conditionals in ironbrew close

--- ironbrew_devirtualizer/vm-handler/identifiers/close_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/close_identifier.hpp
@@ -9,6 +9,11 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			if (auto for_loop = path->find_first_of<ir::statement::for_step>(); for_loop.has_value()) {
 				if (for_loop->get().init->to_string() == "1" && !for_loop->get().step.has_value()) {
+					if (auto unary = for_loop->get().end->as<ir::expression::unary_expression>()) {
+						if (unary->operation == ir::expression::unary_expression::operation_t::len) {
+							return vm_arch::opcode::op_close;
+						}
+					}
 				}
 			}
 
