8634cb6c539ee2acd7115f13ce77c73e0a2fd8a6
JanSafronov <jansafronov@gmail.com>
2023-07-14 01:48:42 +0300

    binary for concat identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/concat_identfier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/concat_identfier.hpp
@@ -9,6 +9,10 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			auto for_step = path->find_first_of<ir::statement::for_step>();
 			if (for_step.has_value()) {
+				auto binary_exp = for_step->get().body->find_first_of<ir::expression::binary_expression>();
+				if (binary_exp.has_value() && binary_exp->get().operation == ir::expression::binary_expression::operation_t::concat) {
+					return vm_arch::opcode::op_concat;
+				}
 			}
 
 			return __super::handle(path);
