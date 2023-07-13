cdab418dc972ee7561c942a85ffea279b161b70e
JanSafronov <jansafronov@gmail.com>
2023-07-14 01:49:24 +0300

    Conditionals for add identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/add_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/add_identifier.hpp
@@ -22,7 +22,16 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 				const auto assign_string = path->body.at(0)->to_string();
 
 				if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b + instruction_opcode_c )") {
-					
+					return vm_arch::opcode::op_add3;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] + instruction_opcode_c )") {
+					return vm_arch::opcode::op_add2;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b + stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_add1;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] + stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_add;
 				}
 			}
 
