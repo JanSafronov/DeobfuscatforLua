cf1fc087b58608bfb6fcc8c74162dd50191793d5
JanSafronov <jansafronov@gmail.com>
2023-07-14 01:48:57 +0300

    Setup closure conditionals

--- ironbrew_devirtualizer/vm-handler/identifiers/closure_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/closure_identifier.hpp
@@ -11,6 +11,12 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 			if (func_call.has_value()) {
 				const auto call_dest = func_call->get().name.value()->to_string();
 				std::cout << "calldest:" << call_dest << std::endl;
+				if (call_dest == "setmetatable") {
+					return vm_arch::opcode::op_closure;
+				}
+				else if (call_dest == "wrap") {
+					return vm_arch::opcode::op_closure1;
+				}
 			}
 
 			return __super::handle(path);
