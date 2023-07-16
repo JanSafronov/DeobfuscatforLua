f01fa9034cc302f329c451adc0e9394d6781c833
JanSafronov <jansafronov@gmail.com>
2023-07-16 16:53:33 +0300

    Configure node typology

--- ast/ir/node.hpp
+++ ast/ir/node.hpp
@@ -26,5 +26,65 @@ namespace deobf::ast::ir {
 			return { };
 		};
 
+		template <typename node_type>
+		std::vector<std::reference_wrapper<node_type>> get_children_type() const { // somewhat static polymorphism doesn't apply to this template method
+			std::vector<std::reference_wrapper<node_type>> results;
+
+			const auto children = get_children();
+			for (const auto& current_node : children) {
+				if (auto result = dynamic_cast<node_type*>(current_node.get())) {
+					results.emplace_back(*result);
+				}
+
+				/*const auto next_children = current_node.get()->get_children_type<node_type>();
+				for (auto& children : next_children)
+					results.emplace_back(children);*/
+				//std::move(next_children.begin(), next_children.end(), std::back_inserter(results));
+			}
+
+			return results;
+		}
+
+		virtual void accept(abstract_visitor_pattern* visitor) = 0;
+
+		template <typename node_type>
+		std::optional<std::reference_wrapper<node_type>> find_first_of() const {
+			const auto children = get_children();
+			for (const auto& current_node : children) { // depth first traversal
+				if (auto return_node = dynamic_cast<node_type*>(current_node.get())) { // dynamic_cast<node_type&> cant be casted into bool smh
+					return *return_node;
+				}
+				else if (auto found_node = current_node.get()->find_first_of<node_type>()) {
+					return found_node;
+				}
+			}
+
+			return std::nullopt;
+		}
+		
+		template <typename node_type>
+		auto as() {
+			return std::dynamic_pointer_cast<node_type>(shared_from_this());
+		}
+
+		template <typename node_type>
+		const auto as() const { // support for const nodes instead of const_cast
+			return std::dynamic_pointer_cast<const node_type>(shared_from_this());
+		}
+
+		template <typename node_type>
+		auto is() const {
+			return dynamic_cast<const node_type*>(this) != nullptr; // performance-wise this would be better than using a dynamic_pointer_cast since it won't create a temp shared_ptr on failure
+		}
+		
+		// move ctor & etc rules
+		node() = default;
+		node(node&&) = default;
+		node(const node&) = delete;
+
+		node& operator=(node&&) = default;
+		node& operator=(const node&) = delete;
+
+		virtual ~node(void) noexcept = default; // abstract ctor (set default)
 	};
 }
