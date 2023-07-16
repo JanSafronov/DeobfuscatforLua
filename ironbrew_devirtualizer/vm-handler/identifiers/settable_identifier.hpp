6cd72b7b6c77d411ac29c266bf08d0bccab50bbb
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:38:29 +0300

    Initialize table settings identifier handling

--- ironbrew_devirtualizer/vm-handler/identifiers/settable_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/settable_identifier.hpp
@@ -7,6 +7,18 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 
 	struct settable_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
+			if (path->body.size() == 1) {
+				if (auto assign = path->body.at(0)->as<ir::statement::variable_assign>()) {
+					const auto assign_string = assign->to_string();
+
+					const auto is_stk = (assign_string == "stack[instruction_opcode_a][stack[instruction_opcode_b]] = stack[instruction_opcode_c]");
+					const auto is_kb = (assign_string == "stack[][instruction_opcode_b] = stack[]");
+
+					if (is_stk) {
+						return vm_arch::opcode::op_settable;
+					}
+				}
+			}
 
 			return __super::handle(path);
 		}
