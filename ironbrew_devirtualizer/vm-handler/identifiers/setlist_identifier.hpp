8fd9a1016e30c71a59dc7e011d2b5eb1d05ff572
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:37:13 +0300

    Commit to setlist identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/setlist_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/setlist_identifier.hpp
@@ -7,6 +7,13 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 
 	struct setlist_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
+			if (auto for_step = path->find_first_of<ir::statement::for_step>(); for_step.has_value()) {
+				auto function_call = for_step.value().get().body->find_first_of<ir::expression::function_call>();
+				if (function_call.has_value()) {
+					if (auto function_name = function_call->get().name; function_name.has_value()) {
+					}
+				}
+			}
 
 			return __super::handle(path);
 		}
