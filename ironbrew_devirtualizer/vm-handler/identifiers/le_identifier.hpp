29af5b9e4f6f74d960f90b54f7a9e054c0141a2e
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:55:21 +0300

    Initialize get, jumps and inequality identifiers

---
+++ ironbrew_devirtualizer/vm-handler/identifiers/le_identifier.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../identifier_handler.hpp"
+
+namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
+	using namespace ast;
+
+	struct le_identifier final : public basic_opcode_handler {
+		vm_arch::opcode handle(ir::statement::block* path) override {
+			if (path->body.size() == 1) {
+				if (auto if_stat = path->body.at(0)->as<ir::statement::if_statement>()) {
+					const auto expression_string = if_stat->condition->to_string();
+					if (if_stat->body->body.size() != 1) {
+						return __super::handle(path);
+					}
+				}
+			}
+
+			return __super::handle(path);
+		}
+	};
+}
