e45478195e3c50d64f34b2632db69c8a089c6e1b
JanSafronov <jansafronov@gmail.com>
2023-07-16 17:04:33 +0300

    Setup propagation acception

--- ast/optimization_passes/constant_propagation.cpp
+++ ast/optimization_passes/constant_propagation.cpp
@@ -39,6 +39,41 @@ namespace deobf::ast::optimization_passes {
 			return true;
 		}
 
+		bool accept(ir::statement::block* expression) override {
+			const auto temp_block = current_visitor_block;
+			current_visitor_block = expression; // could also pass as argument?
+
+			for (auto& statement : expression->body) { // todo a function to optimize function calls, this is a literal mess
+				statement->accept(this);
+			}
+
+			if (expression->ret.has_value()) {
+				const auto ret_value = expression->ret.value();
+
+				// first ret got some shitty call, populate the symbol table with the pushed parameters (this is a part of the aztupbrew deobfuscator)
+				/*std::call_once(has_visited_return, [this, &ret_value]() {	
+					auto& function_call = ret_value->find_first_of<ir::expression::function_call>().value().get();
+
+					if (auto function_expression = function_call.name.value()->as<ir::expression::function>()) {
+						if (function_call.arguments.at(0)->body.size() == function_expression->parameters.size()) {
+							for (auto i = 0ul; i < function_call.arguments.size(); ++i) { // zip iterate
+								const auto argument_value = function_call.arguments.at(0)->body.at(i)->to_string();
+								const auto parameter_value = function_expression->parameters.at(i)->to_string();
+								std::cout << "ins:::" << argument_value << std::endl;
+								function_expression->body->insert_symbol(parameter_value, std::make_shared<ir::expression::string_literal>(argument_value));
+							}
+						}
+					}
+				});*/
+
+				ret_value->accept(this);
+			}
+
+			current_visitor_block = temp_block;
+
+			return false;
+		}
+
 	private:
 		const ir::statement::block* current_visitor_block = nullptr;
 		//std::once_flag has_visited_return;
