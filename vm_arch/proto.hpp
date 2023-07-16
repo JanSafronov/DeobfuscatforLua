60f701eca406f76d49bb75bb1c21179b513fc23a
JanSafronov <jansafronov@gmail.com>
2023-07-16 17:10:57 +0300

    Init proto

---
+++ vm_arch/proto.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "instruction.hpp"
+#include "constant.hpp"
+#include "opcode.hpp"
+
+#include <vector>
+#include <memory>
+#include <iostream>
+
+namespace deobf::vm_arch {
+	struct proto {
+		std::vector<std::unique_ptr<instruction>> instructions;
+		std::vector<std::unique_ptr<proto>> protos;
+		std::vector<std::int32_t> lineinfo; // load vector we wont need to propagate them into each insn
+		std::vector<std::shared_ptr<constant>> constants;
+
+		std::int8_t num_params;
+		std::int16_t nups;
+		std::size_t max_stack_size;
+		std::int8_t is_vararg;
+
+		std::string print_chunk() const;
+
+		virtual ~proto() = default;
+	};
+}
