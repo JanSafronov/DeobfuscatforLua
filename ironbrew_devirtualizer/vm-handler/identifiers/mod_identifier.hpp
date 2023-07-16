e3708422a7f354b93f29e5b1de0f3c9921c0d6e1
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:49:54 +0300

    Handle stack instructions modularity

--- ironbrew_devirtualizer/vm-handler/identifiers/mod_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/mod_identifier.hpp
@@ -12,6 +12,15 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 				if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b % instruction_opcode_c )") {
 					return vm_arch::opcode::op_mod3;
 				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] % instruction_opcode_c )") {
+					return vm_arch::opcode::op_mod2;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b % stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_mod1;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] % stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_mod;
+				}
 			}
 
 			return __super::handle(path);
