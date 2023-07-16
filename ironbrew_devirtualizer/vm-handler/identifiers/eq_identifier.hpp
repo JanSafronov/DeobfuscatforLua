f40b9cf58357a0a6c08fd938afdb61aa70b8b93f
JanSafronov <jansafronov@gmail.com>
2023-07-16 15:00:15 +0300

    Handle equality and for prep identifiers

--- ironbrew_devirtualizer/vm-handler/identifiers/eq_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/eq_identifier.hpp
@@ -8,6 +8,20 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 	struct eq_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			if (path->body.size() == 1) {
+				if (auto if_stat = path->body.at(0)->as<ir::statement::if_statement>()) {
+					const auto expression_string = if_stat->condition->to_string();
+
+					// eq
+					if (expression_string == "( instruction_opcode_a == instruction_opcode_c )") {
+						return vm_arch::opcode::op_eq3;
+					}
+
+					// ne
+
+					if (expression_string == "( instruction_opcode_a ~= instruction_opcode_c )") {
+						return vm_arch::opcode::op_ne3;
+					}
+				}
 			}
 
 			return __super::handle(path);
--- ironbrew_devirtualizer/vm-handler/identifiers/forprep_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/forprep_identifier.hpp
@@ -9,6 +9,16 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			auto if_stat = path->find_first_of<ir::statement::if_statement>();
 			if (if_stat.has_value()) {
+				if (auto condition = if_stat->get().condition->as<ir::expression::binary_expression>()) {
+					if (condition->operation == ir::expression::binary_expression::operation_t::gt && condition->right->to_string() == "0") {
+						if (auto symbol = path->find_symbol(condition->left->to_string())) {
+							if (symbol->symbol_value->to_string() == "stack[( instruction_opcode_a + 2 )]") {
+
+								return vm_arch::opcode::op_forprep;
+							}
+						}
+					}
+				}
 			}
 			
 			return __super::handle(path);
