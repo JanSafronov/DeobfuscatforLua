73d5ed47f7203ad5e31884bfc8c7650c61fd6b55
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:45:55 +0300

    Handling power stack identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/pow_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/pow_identifier.hpp
@@ -12,6 +12,15 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 				if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b ^ instruction_opcode_c )") {
 					return vm_arch::opcode::op_pow3;
 				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] ^ instruction_opcode_c )") {
+					return vm_arch::opcode::op_pow2;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( instruction_opcode_b ^ stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_pow1;
+				}
+				else if (assign_string == "stack[instruction_opcode_a] = ( stack[instruction_opcode_b] ^ stack[instruction_opcode_c] )") {
+					return vm_arch::opcode::op_pow;
+				}
 			}
 
 			return __super::handle(path);
