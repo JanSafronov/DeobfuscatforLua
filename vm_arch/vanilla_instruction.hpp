969f0b584e03f6da4c058fdd3384c9c92a3ee074
JanSafronov <jansafronov@gmail.com>
2023-07-16 17:12:35 +0300

    complete vanilla instructions

--- vm_arch/vanilla_instruction.hpp
+++ vm_arch/vanilla_instruction.hpp
@@ -31,6 +31,38 @@ namespace deobf::vm_arch {
 		};
 #pragma pack(pop)*/
 
+		std::size_t line_defined;
+		vm_arch::vanilla_opcode opcode;
+		vm_arch::vanilla_instruction_type type;
+
+		//std::uint8_t a;
+		//std::uint8_t b;
+		//std::uint8_t c;
+		//std::int16_t sbx;
+		//std::uint16_t bx;
+
+		std::int32_t a;
+		std::int32_t b;
+		std::int32_t c;
+		std::int32_t sbx;
+		std::int32_t bx;
+
+		explicit vanilla_instruction() : a(0), b(0), c(0), sbx(0), bx(0), line_defined(0){ };
+		
+		std::uint32_t get_value() const;
+		explicit vanilla_instruction(const vanilla_instruction& other) :
+			opcode(other.opcode),
+			a(other.a),
+			b(other.b),
+			c(other.c),
+			sbx(other.sbx),
+			bx(other.bx),
+			type(other.type),
+			line_defined(other.line_defined)
+		{ };
+
+
+		vanilla_instruction& operator=(const vanilla_instruction&) = delete;
 
 		virtual ~vanilla_instruction() = default;
 	};
