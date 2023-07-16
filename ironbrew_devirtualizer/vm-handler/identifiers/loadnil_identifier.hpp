d74ede11bb19270c9647cac597b8aabfb21401a8
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:51:18 +0300

    Initialize load identifiers

---
+++ ironbrew_devirtualizer/vm-handler/identifiers/loadnil_identifier.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../identifier_handler.hpp"
+
+namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
+	using namespace ast;
+
+	struct loadnil_identifier final : public basic_opcode_handler {
+		vm_arch::opcode handle(ir::statement::block* path) override {
+
+			return __super::handle(path);
+		}
+	};
+}
