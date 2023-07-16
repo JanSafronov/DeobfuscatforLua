5c3b618499432d628a3b4d469889f2727c8470ca
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:39:41 +0300

    Setup upvalues identifier handling

--- ironbrew_devirtualizer/vm-handler/identifiers/setupval_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/setupval_identifier.hpp
@@ -7,6 +7,15 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 
 	struct setupval_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
+			if (path->body.size() == 1) {
+				if (auto assign = path->body.at(0)->as<ir::statement::variable_assign>()) {
+					const auto assign_string = assign->to_string();
+					if (assign_string == "upvalues[instruction_opcode_b] = stack[instruction_opcode_a]") {
+						std::cout << "setupval\n";
+						return vm_arch::opcode::op_setupval;
+					}
+				}
+			}
 
 			return __super::handle(path);
 		}
