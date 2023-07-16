89f2854a8e205f81d454546ead298967feb620f2
JanSafronov <jansafronov@gmail.com>
2023-07-16 15:45:53 +0300

    Setup stack reading utility

--- ironbrew_devirtualizer/symbolic-execution/deserializer/compression_utilities.cpp
+++ ironbrew_devirtualizer/symbolic-execution/deserializer/compression_utilities.cpp
@@ -13,6 +13,29 @@ namespace deobf::ironbrew_devirtualizer::symbolic_execution::deserializer {
             conversion_table.emplace_back(1, static_cast<char>(i));
 
         static const auto base36_map = std::string{ "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+        auto read_compressed_partial = [&compression_stream]() -> long {
+            const auto read_offset = base36_map.find(static_cast<char>(compression_stream.get()));
+            if (read_offset == -1)
+                return -1;
+
+            auto data_block = std::make_unique<char[]>(read_offset);
+            compression_stream.read(data_block.get(), read_offset);
+
+            return strtol(data_block.get(), nullptr, 36);
+        };
+
+        {
+            std::string current_character(1, static_cast<char>(read_compressed_partial()));
+            for (std::string decompression_data; compression_stream.good(); result << current_character, current_character = decompression_data) {
+                long current_writer = read_compressed_partial();
+                if (current_writer < conversion_table.size())
+                    decompression_data = conversion_table[current_writer];
+                else
+                    decompression_data = current_character + current_character.front(); // todo: .front() might result in UB?
+
+                conversion_table.push_back(current_character + decompression_data.front());
+            }
+        }
 
         return result.str();
     }
