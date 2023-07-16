4e5691ef7ac61b4a2102966eb39f3241a072c445
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:34:42 +0300

    handle testing conditions

--- ironbrew_devirtualizer/vm-handler/identifiers/test_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/test_identifier.hpp
@@ -9,6 +9,17 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			if (path->body.size() == 1) {
 				if (auto test_statement = path->find_first_of<ir::statement::if_statement>()) {
+					auto condition = test_statement.value().get().condition->to_string();
+
+					bool is_c0 = (condition == "stack[instruction_opcode_a]");
+					bool is_c1 = (condition == "not( stack[instruction_opcode_a] )");
+
+					if (is_c0) {
+						return vm_arch::opcode::op_test;
+					}
+					else if (is_c1) {
+						return vm_arch::opcode::op_test1;
+					}
 				}
 			}
 
