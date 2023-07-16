b28cf60a9a16662eee7b6e3c10bc02dbf2a5ea50
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:51:42 +0300

    Handle 'it' identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/lt_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/lt_identifier.hpp
@@ -8,6 +8,14 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 	struct lt_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			if (path->body.size() == 1) {
+				if (auto if_stat = path->body.at(0)->as<ir::statement::if_statement>()) {
+					const auto expression_string = if_stat->condition->to_string();
+					if (if_stat->body->body.size() != 1) {
+						return __super::handle(path);
+					}
+
+					bool is_ge = (if_stat->body->body.at(0)->to_string() != "instruction_pointer = instruction_opcode_b");
+				}
 			}
 
 			return __super::handle(path);
