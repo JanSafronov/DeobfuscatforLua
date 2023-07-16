5ecfa1ce17d2ef17eec805a277cc5fd1524a6204
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:40:16 +0300

    Initialize tail call handling

--- ironbrew_devirtualizer/vm-handler/identifiers/tailcall_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/tailcall_identifier.hpp
@@ -7,6 +7,13 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 
 	struct tailcall_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
+			auto do_statement = path->find_first_of<ir::statement::do_block>();
+			if (do_statement.has_value()) {
+				auto& body_wrapper = do_statement->get();
+				std::cout << "dostat\n";
+				if (body_wrapper.body->body.size() == 0) {
+				}
+			}
 
 			return __super::handle(path);
 		}
