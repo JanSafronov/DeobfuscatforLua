68dd40f56a4fd8d81f494340e09e8df98b5b2a02
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:54:04 +0300

    Handle stack instructions length

--- ironbrew_devirtualizer/vm-handler/identifiers/len_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/len_identifier.hpp
@@ -7,6 +7,12 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 	struct len_identifier final : public basic_opcode_handler{
 		vm_arch::opcode handle(ir::statement::block* path) override {
 			if (path->body.size() == 1) {
+				if (auto assign = path->body.at(0)->as<ir::statement::variable_assign>()) {
+					const auto assign_string = assign->to_string();
+					if (assign_string == "stack[instruction_opcode_a] = #( stack[instruction_opcode_b] )") {
+						return vm_arch::opcode::op_len;
+					}
+				}
 			}
 
 			return __super::handle(path);
