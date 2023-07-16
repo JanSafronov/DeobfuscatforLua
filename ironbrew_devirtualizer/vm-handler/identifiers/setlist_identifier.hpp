fc3291839e9cd37721f707ef3684d97a9bf33727
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:37:36 +0300

    handle setting list identifier

--- ironbrew_devirtualizer/vm-handler/identifiers/setlist_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/setlist_identifier.hpp
@@ -11,6 +11,21 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 				auto function_call = for_step.value().get().body->find_first_of<ir::expression::function_call>();
 				if (function_call.has_value()) {
 					if (auto function_name = function_call->get().name; function_name.has_value()) {
+						if (function_name.value()->to_string() == "table.insert") {
+							if (auto result = path->find_first_of<ir::statement::variable_assign>(); result.has_value()) {
+								if (result.value().get().to_string() == "instruction_pointer = ( instruction_pointer + 1 )") {
+									return vm_arch::opcode::op_setlist2;
+								}
+							}
+
+							if (auto variable = for_step.value().get().end->as<ir::expression::variable>()) {
+								if (variable->to_string() != "instruction_opcode_b") { // top
+									return vm_arch::opcode::op_setlist1;
+								}
+							}
+
+							return vm_arch::opcode::op_setlist;
+						}
 					}
 				}
 			}
