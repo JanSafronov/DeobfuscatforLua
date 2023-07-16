301737d7d75feb997d96517b26f2f0f999b1102a
JanSafronov <jansafronov@gmail.com>
2023-07-16 14:33:07 +0300

    handle stack instruction code

--- ironbrew_devirtualizer/vm-handler/identifiers/unm_identifier.hpp
+++ ironbrew_devirtualizer/vm-handler/identifiers/unm_identifier.hpp
@@ -7,6 +7,11 @@ namespace deobf::ironbrew_devirtualizer::vm_handler_identifiers {
 
 	struct unm_identifier final : public basic_opcode_handler {
 		vm_arch::opcode handle(ir::statement::block* path) override {
+			if (path->body.size() == 1) {
+				if (path->body.at(0)->to_string() == "stack[instruction_opcode_a] = -( stack[instruction_opcode_b] )") {
+					return vm_arch::opcode::op_unm;
+				}
+			}
 
 			return __super::handle(path);
 		}
